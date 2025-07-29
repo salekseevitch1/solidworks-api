8a396cd0671c5f43d258dcb4ecaa8a6be2e8d465
SerhiiPietukhov <serg.p@parametric.center>
2025-07-29 18:37:58 +0300

    Add CreateCircle method to SketchManager class
    - Introduced `CreateCircle` method that accepts a center point and diameter.
    - Inserts a sketch and creates a circle based on the provided parameters.
    - Returns the created `SketchSegment`.
    - Manages `AddToDB` state for proper database handling during sketch creation.

--- SolidDna/CADBooster.SolidDna/SolidWorks/SketchManager/SketchManager.cs
+++ SolidDna/CADBooster.SolidDna/SolidWorks/SketchManager/SketchManager.cs
@@ -22,4 +22,27 @@ public class SketchManager(global::SolidWorks.Interop.sldworks.SketchManager com
 
         return sketchPoint;
     }
+
+    public SketchSegment CreateCircle(XYZ center, double diameter)
+    {
+        UnsafeObject.InsertSketch(true);
+
+        UnsafeObject.AddToDB = true;
+
+        var pointOnCircle = center.MoveAlongVector(XYZ.BasisX, diameter / 2);
+
+        var sketchSegment = UnsafeObject.CreateCircle(
+            center.X,
+            center.Y,
+            center.Z,
+            pointOnCircle.X,
+            pointOnCircle.Y,
+            pointOnCircle.Z);
+
+        UnsafeObject.AddToDB = false;
+
+        UnsafeObject.InsertSketch(false);
+
+        return sketchSegment;
+    }
 }
